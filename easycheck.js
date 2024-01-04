39461f9f5f05444a17207c71998fc2a756c0fb73
[darnack] <[elesmesvallejo@gmail.com]>
2024-01-04 12:04:50 -0500

    Se modifica la implementación por medio del nuevo método EasyCheckInit

--- easycheck.js
+++ easycheck.js
@@ -1,44 +1,61 @@
-﻿/* EasyCheck JS 1.4.13
-** Created by Elkin Lesmes, 2015-09-23
-** Allows validate any field in a web form with multiple rules that can be easyly added in the class attribute of the fields.
-** Works with multiple forms.
-** Update (2020-01-31): 
-        * Multiple message on same box
-        * Fix regex text rule
-        * New checked, password, and dateformat rules
-        * DOM psedo-virtualization
-        * Real time validation
-        * Auto spaces remove function
-** Update (2020-10-09): 
-	 * New distinct validation rule     
-** Last update (2021-01-15):
-	 * New money formatter
-     * Messages customizer
+﻿/* EasyCheck JS
+** Created by Elkin Lesmes, 2015-09-23  
+** Last update (2023-07-21):
+	 * Adds EasyCheckInit global method
 */
 
-var EasyCheck = function (container) {
+window.EasyCheckInit = function () {
+    let forms = $('form.validate');
+    let validators = [];
 
-    var my = {};
-    var fields = [];
-    var valid = true;
-    var count = 0;
-    my.container = container;
+    forms.each(function () {
+        validators.push(new EasyCheck($(this)));
+    });
 
-    var constructor = function () {
-        // get all fields from container
-        var domFields = $('#' + my.container).find('[class*=\'validate\']');
+    return validators;
+}
+
+const EasyCheck = function (form) {
+
+    let my = {};
+    let fields = [];
+    let valid = true;
+    let count = 0;    
+    let formId = '';
+    my.form = form;
+
+    let constructor = function () {
+
+        formId = Math.random().toString().replace('.', '');
+        $(my.form).attr('id', formId);
+
+        // get all fields from form
+        let domFields = $(my.form).find('[class*=\'validate\']');        
         // add fields to control list
-        for (var i = 0; i < domFields.length; i++) {
-            addField($(domFields[i]));
+        for (const element of domFields) {
+            addField($(element));
         }
+
+        let button = $(my.form).find('input.validator, button.validator, a.validator');
+
+        if (button == undefined || button.length == 0)
+            throw new Error('Se requiere un botón de validación')
+
+        $(button[0]).on('click', function (e) {
+            
+            if (!my.validate()) {
+                e.stopImmediatePropagation();
+                e.preventDefault();
+            }
+        });
     }
 
-    var addField = function ($field) {
+    let addField = function ($field) {
         // extract rules
-        var sClass = $field.attr('class');
-        var start = sClass.indexOf('{') + 1;
-        var end = sClass.indexOf('}');
-        var rules = sClass.substring(start, end).split(',');
+        let sClass = $field.attr('class');
+        let start = sClass.indexOf('{') + 1;
+        let end = sClass.indexOf('}');
+        let rules = sClass.substring(start, end).split(',');
         // add event listener for fields            
         $field.change(function () {
             validateEvent(this);
@@ -48,10 +65,10 @@ var EasyCheck = function (container) {
         });
         // asigns id by default
         if ($field.attr('id') === undefined) {
-            $field.attr('id', container + '_field' + (fields.length + count));
+            $field.attr('id', formId + '_field' + (fields.length + count));
         }
         // construct field object
-        var field = {
+        let field = {
             id: $field.attr('id'),
             tagName: $field.prop('tagName').toLowerCase(),
             type: $field.prop('type'),
@@ -66,8 +83,8 @@ var EasyCheck = function (container) {
         fields[field.id] = field;
         count++;
         // add remove space event
-        for (var i = 0; i < field.rules.length; i++) {
-            var rule = field.rules[i].trim();
+        for (let i = 0; i < field.rules.length; i++) {
+            let rule = field.rules[i].trim();
             if (rule === "digit" || rule === "number" || rule === "email" || rule === "password" || rule === "dateformat") {
                 $field.on('input', function () {
                     this.value = this.value.replace(/[ ]/g, '');
@@ -84,9 +101,7 @@ var EasyCheck = function (container) {
         // establishes focus on first error field
         for (id in fields) {
             if (!fields[id].valid) {
-                $('html,body').animate({ scrollTop: fields[id].$object.offset().top }, 500, function () {
-                    fields[id].$object.focus();
-                });
+                window.scrollTo({ top: fields[id].$object.offset().top, behavior: 'smooth' });
                 break;
             }
         }
@@ -114,29 +129,52 @@ var EasyCheck = function (container) {
         fields[id].enabled = true;
     }
 
-    my.customMessages = function (id, collecion) {
+    my.removeRule = function (id, rule) {
+        fields[id].rules = arrayRemove(fields[id].rules, rule);        
+    }
+
+    my.addRule = function (id, rule) {
+        fields[id].rules = arrayInsert(fields[id].rules, rule);        
+    }
+
+    my.customMessages = function (id, collection) {
         try {
-            for (const prop in collecion)
+            for (const prop in collection)
             {
-                fields[id].message[prop] = collecion[prop];
+                fields[id].message[prop] = collection[prop];
             }
         }
         catch { }
     }
 
-    var validateEvent = function (domField) {
+    function arrayRemove(arr, value) {        
+        return arr.filter(function (ele) {
+            return ele != value;
+        });
+    }
+
+    function arrayInsert(arr, value) {
+        let items = arr.filter(function (ele) {
+            return ele != value;
+        });
+        items.push(value);
+        return items;
+    }
+
+    let validateEvent = function (domField) {
         validateField(fields[$(domField).attr('id')]);
     }
 
-    var validateField = function (field) {
+    let validateField = function (field) {
         try {
             field.valid = true; // restart field flag
-            field.value = field.$object.val().trim(); // refresh field value
+            const type = typeof field.$object.val();
+            field.value = type === 'string' ? field.$object.val().trim() : field.$object.val(); // refresh field value
             // restart styles
             $("span[data-id='" + field.id + "_error']").remove();
             field.$object.removeClass("error");
             // apply rules
-            for (var i = 0; i < field.rules.length; i++) {
+            for (let i = 0; i < field.rules.length; i++) {
                 // apply simple rules
                 switch (field.rules[i].trim()) {
                     case "required": required(field); break;
@@ -151,10 +189,10 @@ var EasyCheck = function (container) {
                     case "password": password(field); break;
                     default:
                         // extract function properties
-                        var start = field.rules[i].indexOf('(') + 1;
-                        var end = field.rules[i].indexOf(')');
-                        var func = field.rules[i].substring(0, field.rules[i].indexOf('('));
-                        var param = field.rules[i].substring(start, end).trim();
+                        let start = field.rules[i].indexOf('(') + 1;
+                        let end = field.rules[i].indexOf(')');
+                        let func = field.rules[i].substring(0, field.rules[i].indexOf('('));
+                        let param = field.rules[i].substring(start, end).trim();
                         // apply function rules
                         switch (func.trim()) {
                             case "equals": equals(field, param); break;
@@ -169,14 +207,14 @@ var EasyCheck = function (container) {
                         ; break;
                 }
             }
-        } catch (e) {
+        } catch(e) {
             valid = false;
             field.valid = valid;
             message(field, "*Error en reglas de validación del campo. ");
         }
     }
 
-    var required = function (field) {
+    let required = function (field) {
         if ((field.value.length == 0 || (field.tagName === "select" && field.value == 0)) && field.enabled) {
             valid = false;
             field.valid = valid;
@@ -184,7 +222,7 @@ var EasyCheck = function (container) {
         }
     }
 
-    var checked = function (field) {
+    let checked = function (field) {
         if (!field.$object.prop("checked") && field.enabled) {
             valid = false;
             field.valid = valid;
@@ -192,9 +230,9 @@ var EasyCheck = function (container) {
         }
     }
 
-    var digit = function (field) {
+    let digit = function (field) {
         if (field.value.length > 0 && field.enabled) {
-            var regex = /^(\d+)$/;
+            let regex = /^(\d+)$/;
             if (!regex.test(field.value)) {
                 valid = false;
                 field.valid = valid;
@@ -203,40 +241,47 @@ var EasyCheck = function (container) {
         }
     }
 
-    var money = function (field) {
+    let money = function (field) {
         if (field.value.length > 0 && field.enabled) {
             
-            var input_val = field.value.replace(/^0+/g, "");
+            let formatted_value = field.value.replace(/^0+/g, "0"); // elimina ceros consecutivos al comienzo
+            formatted_value = formatted_value.replace(/^0[^,]/g, "0"); // no permite escritura de ceros a la izquierda
+            formatted_value = formatted_value.replace(/^,*/g, ""); // elimina comas al comienzo
 
             // check for decimal
-            if (input_val.indexOf(".") >= 0) {
+            if (formatted_value.indexOf(",") >= 0) {
 
                 // get position of first decimal this prevents multiple decimals from being entered
-                var decimal_pos = input_val.indexOf(".");
+                let decimal_pos = formatted_value.indexOf(",");
                 // split number by decimal point
-                var left_side = input_val.substring(0, decimal_pos);
-                var right_side = input_val.substring(decimal_pos);
+                let left_side = formatted_value.substring(0, decimal_pos);
+                let right_side = formatted_value.substring(decimal_pos);
                 // add commas to left side of number
-                left_side = left_side.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                left_side = left_side.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
                 // validate right side
-                right_side = right_side.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                right_side = right_side.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
                 // Limit decimal to only 2 digits
                 right_side = right_side.substring(0, 2);
                 // join number by .
-                input_val = left_side + "." + right_side;
+                formatted_value = left_side + "," + right_side;
 
             } else {
-                input_val = input_val.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                formatted_value = formatted_value.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
             }
 
-            field.$object.val(input_val);
+            field.$object.val(formatted_value);
+
+            let cleanValue = formatted_value.split('.').join('');
+            cleanValue = cleanValue.split(',').join('.');
+
+            field.value = cleanValue;
             field.valid = true;
         }
     }
 
-    var number = function (field) {
+    let number = function (field) {
         if (field.value.length > 0 && field.enabled) {
-            var regex = /^(?:-?\d+|-?\d{1,3}(?:,\d{3})+)?(?:\.\d +)?$/;
+            let regex = /^(?:-?\d+|-?\d{1,3}(?:,\d{3})+)?(?:\.\d +)?$/;
             if (!regex.test(field.value)) {
                 valid = false;
                 field.valid = valid;
@@ -245,9 +290,9 @@ var EasyCheck = function (container) {
         }
     }
 
-    var decimal = function (field) {
+    let decimal = function (field) {
         if (field.value.length > 0 && field.enabled) {
-            var regex = /^(\d*\,)?\d+$/;
+            let regex = /^(\d*\,)?\d+$/;
             if (!regex.test(field.value)) {
                 valid = false;
                 field.valid = valid;
@@ -256,9 +301,9 @@ var EasyCheck = function (container) {
         }
     }
 
-    var text = function (field) {
+    let text = function (field) {
         if (field.value.length > 0 && field.enabled) {
-            var regex = /^([a-zA-ZÑñáéíóúÁÉÍÓÚ]+(?: [a-zA-ZñÑáéíóúÁÉÍÓÚ]+)*)$/;
+            let regex = /^([a-zA-ZÑñáéíóúÁÉÍÓÚ]+(?: [a-zA-ZñÑáéíóúÁÉÍÓÚ]+)*)$/;
             if (!regex.test(field.value)) {
                 valid = false;
                 field.valid = valid;
@@ -267,9 +312,9 @@ var EasyCheck = function (container) {
         }
     }
 
-    var alphanumeric = function (field) {
+    let alphanumeric = function (field) {
         if (field.value.length > 0 && field.enabled) {
-            var regex = /^([a-zA-ZÑñáéíóúÁÉÍÓÚ0-9]+(?: [a-zA-ZñÑáéíóúÁÉÍÓÚ0-9]+)*)$/;
+            let regex = /^([a-zA-ZÑñáéíóúÁÉÍÓÚ0-9]+(?: [a-zA-ZñÑáéíóúÁÉÍÓÚ0-9]+)*)$/;
             if (!regex.test(field.value)) {
                 valid = false;
                 field.valid = valid;
@@ -278,9 +323,9 @@ var EasyCheck = function (container) {
         }
     }
 
-    var email = function (field) {
+    let email = function (field) {
         if (field.value.length > 0 && field.enabled) {
-            var regex = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*[.]([a-z]{2,3})$/;
+            let regex = /^[_a-zA-Z0-9-]+(.[_a-zA-Z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*[.]([a-z]{2,3})$/;
             if (!regex.test(field.value.toLowerCase().trim())) {
                 valid = false;
                 field.valid = valid;
@@ -289,18 +334,18 @@ var EasyCheck = function (container) {
         }
     }
 
-    var password = function (field) {
+    let password = function (field) {
         if (field.value.length > 0 && field.enabled) {
-            var regex = /^(?=.*?[A-Z])(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{6,}$/;
+            let regex = /^(?=.*?[A-Z])(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/;
             if (!regex.test(field.value.trim())) {
                 valid = false;
                 field.valid = valid;
-                message(field, field.message["password"] || "Se requiere al menos 6 caracteres, una mayúscula, un número y un caracter especial.");
+                message(field, field.message["password"] || "La contraseña debe tener mínimo 8 caracteres que incluya una mayúscula, un número y un carácter especial");
             }
         }
     }
 
-    var equals = function (field, field2) {
+    let equals = function (field, field2) {
         if (field.value != $("#" + field2).val() && field.enabled) {
             valid = false;
             field.valid = valid;
@@ -308,7 +353,7 @@ var EasyCheck = function (container) {
         }
     }
 
-    var distinct = function (field, field2) {
+    let distinct = function (field, field2) {
         if (field.value == $("#" + field2).val() && field.enabled) {
             valid = false;
             field.valid = valid;
@@ -316,44 +361,41 @@ var EasyCheck = function (container) {
         }
     }
 
-    var minlength = function (field, size) {
+    let minlength = function (field, size) {
         if (field.value.length > 0 && field.enabled && field.value.length < size) {
             valid = false;
             field.valid = valid;
-            message(field, "Debe contener al menos " + size + " caracteres. ");
+            message(field, field.message["minlength"] || "Debe contener al menos " + size + " caracteres. ");
         }
     }
 
-    var maxlength = function (field, size) {
+    let maxlength = function (field, size) {
         if (field.value.length > size && field.enabled) {
             valid = false;
             field.valid = valid;
-            message(field, "No puede contener más de " + size + " caracteres. ");
+            message(field, field.message["maxlength"] || "No puede contener más de " + size + " caracteres. ");
         }
     }
 
-    var minvalue = function (field, value) {
-
-        if (field.value.length > 0 && field.enabled && parseInt(field.value) <= value) {
-
+    let minvalue = function (field, value) {
+        if (field.value.length > 0 && field.enabled && parseFloat(field.value) <= parseFloat(value)) {
             valid = false;
             field.valid = valid;
-            message(field, "El valor debe ser mayor a " + value);
+            message(field, field.message["minvalue"] || "El valor debe ser mayor a " + value);
         }
     }
 
-    var maxvalue = function (field, value) {
-
-        if (field.value.length > 0 && field.enabled && parseInt(field.value) >= value) {
+    let maxvalue = function (field, value) {
+        if (field.value.length > 0 && field.enabled && parseFloat(field.value) >= parseFloat(value)) {
             valid = false;
             field.valid = valid;
-            message(field, "El valor debe ser inferior a " + value);
+            message(field, field.message["maxvalue"] || "El valor debe ser inferior a " + value);
         }
     }
 
-    var dateformat = function (field, format) {
+    let dateformat = function (field, format) {
         if (field.value.length > 0 && field.enabled) {
-            var exp = format.toLowerCase();
+            let exp = format.toLowerCase();
             exp = exp.replace(/dd/g, '(\\#{2})');
             exp = exp.replace(/d/g, '(\\#{1,2})');
             exp = exp.replace(/mm/g, '(\\#{2})');
@@ -373,17 +415,17 @@ var EasyCheck = function (container) {
                     format = format.replace(/mm/, '#').replace(/m/, 'mm').replace(/#/, 'mm');
                 }
                 // day
-                var a = format.match('d{1,2}').index;
-                var b = format.match('d{1,2}')[0].length;
-                var day = parseInt(field.value.substring(a, a + b));
+                let a = format.match('d{1,2}').index;
+                let b = format.match('d{1,2}')[0].length;
+                let day = parseInt(field.value.substring(a, a + b));
                 //month
                 a = format.match('m{1,2}').index;
                 b = format.match('m{1,2}')[0].length;
-                var month = parseInt(field.value.substring(a, a + b));
+                let month = parseInt(field.value.substring(a, a + b));
                 //year
                 a = format.match('y{2,4}').index;
                 b = format.match('y{2,4}')[0].length;
-                var year = parseInt(field.value.substring(a, a + b));
+                let year = parseInt(field.value.substring(a, a + b));
 
                 if (month == 4 || month == 6 || month == 9 || month == 11) {
                     if (day > 30) {
@@ -409,11 +451,11 @@ var EasyCheck = function (container) {
         }
     }
 
-    var message = function (field, text) {
+    let message = function (field, text) {
         if ($("span[data-id='" + field.id + "_error']").length > 0) {
             $("span[data-id='" + field.id + "_error']").append('<span class="message">*' + text + '</span>');;
         } else {
-            var top = (field.type === "checkbox") ? 22 : 0;
+            let top = (field.type === "checkbox") ? 22 : 0;
             field.$object.after('<span data-id="' + field.id + '_error" class="error" style="margin-top:' + top + 'px"><span class="message">*' + text + '</span></span>');
             field.$object.addClass('error');
         }
